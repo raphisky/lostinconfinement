f8695efa95501d3c6ca41f74b2f4f2733f137bc7
raph <raphael.kammoun@gmail.com>
2020-03-20 19:45:51 +0100

    there now is a form
    many things who care

--- lostinconfinement.js
+++ lostinconfinement.js
@@ -1,24 +1,79 @@
+// form
+
+$.fn.serializeObject = function()
+{
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
+
+
+var $form = $('form#confinement-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwd31sg_vPl7YvRLxxCNJHXns3M3kVHwFw2aMVXXedo_HplG-I/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).done(
+    // do something
+  );
+})
+
+
+
+// LOAD ACTIVITIES
+
 var confinementThings = [];
 var confinementThingsHour = [];
-var isDataLoaded = false;
 
 
-$.getJSON("https://spreadsheets.google.com/feeds/list/1p1rZkKs7FZ5DrIoG8UYsSSB9cg2pvKCkWLnN4qR-fYQ/od6/public/values?alt=json", function(data) {
+$.getJSON("https://spreadsheets.google.com/feeds/list/1DpU7Fni82j2hXfloJnnmUBmTPbZ0eHQi7FklWcl2-8M/od6/public/values?alt=json", function(data) {
   for (var el in data.feed.entry) {
     confinementThings.push(data.feed.entry[el].gsx$confinementthing.$t);
     confinementThingsHour.push(data.feed.entry[el].gsx$confinementthinghour.$t);
   }
-  isDataLoaded = true;
-  console.log(confinementThings);
   displayConfinementThings();
-
   return confinementThings;
 });
+
+$('#addPrompt').click(function() {
+  $('#confinement-form').show();
+})
+
 
 function displayConfinementThings() {
   for (var i = 0; i < confinementThings.length; i++) {
     var newThing = confinementThings[i];
     var newThingHour = confinementThingsHour[i];
-    $('#confinementThingsContainer').append(newThing + " à " + newThingHour + ", ");
+    var thingSpan = document.createElement('span');
+    thingSpan.innerHTML = newThing + " à " + newThingHour + ", ";
+    spawnConfinementThing(thingSpan);
   }
+}
+
+function spawnAddThingPrompt() {
+    var addThingPrompt = document.createElement('span');
+    addThingPrompt.innerHTML = "et toi ?";
+    $('#confinementThingsContainer').append(addThingPrompt);
+}
+
+function spawnConfinementThing(el) {
+  setTimeout(function() {
+    $('#confinementThingsContainer').append(el);
+    $('span').addClass("thing");
+  }, 300);
 }
